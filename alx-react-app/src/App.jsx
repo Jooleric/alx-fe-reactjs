93c1907664921e450d8cbed1ef0b2067f68884eb
Jooleric <bernardjoseph7872gmail.com>
2025-08-29 20:51:33 +0100

    Update syntax error

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,18 @@
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
-import UserProfile from "./components/UserProfile";
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
-  return (
-     <>
-      <Header />
-      <MainContent />
-      <Footer />
-      <UserProfile name="Benny" age={26} bio="React is cool and fun" />
-     </>
-  );
+    return (
+        <>
+            <WelcomeMessage />
+            <Header />
+            <MainContent />
+            <Footer />
+            <UserProfile name="Benny" age={26} bio="React is cool and fun" />
+        </>
+    );
 }
-
 export default App;
