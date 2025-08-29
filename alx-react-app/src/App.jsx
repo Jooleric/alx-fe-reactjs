950a004171dcc1fee405fc3984077f11db5ab875
Jooleric <bernardjoseph7872gmail.com>
2025-08-29 20:21:55 +0100

    React project

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,17 @@
-import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import './App.css';
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 
 function App() {
-    return (
-        <>
-            <WelcomeMessage />
-            <Header />
-            <MainContent />
-            <Footer />
-        </>
-    );
+  return (
+     <>
+      <Header />
+      <MainContent />
+      <Footer />
+      <UserProfile name="Benny" age={26} bio="React is cool and fun" />
+     </>
+  );
 }
+
 export default App;
---
+++ alx-react-app/src/components/App.jsx
@@ -0,0 +1,20 @@
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfiile';
+import App from "./App.jsx";
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <WelcomeMessage />
+            <Header />
+            <MainContent />
+            <Footer />
+            <UserProfile name="Alice" age={26} bio="React is cool and fun" />
+        </>
+    );
+}
+export default App;
