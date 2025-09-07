02de6504c572ed2367e9b6a146368a8b332c7bcc
Jooleric <bernardjoseph7872gmail.com>
2025-09-07 01:45:09 +0100

    Add modification

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,6 +4,8 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
 import WelcomeMessage from './components/WelcomeMessage';
+import Counter from './components/Counter';
+
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <WelcomeMessage />
       <Footer />
+      <h1>Welcome to My Counter App</h1>
+      <Counter />
     </div>
   );
 }
