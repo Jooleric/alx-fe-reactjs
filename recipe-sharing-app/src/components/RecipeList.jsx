09249b78b5789f26da37a8a89a90613a21c0cddf
Jooleric <bernardjoseph7872gmail.com>
2025-09-11 15:07:46 +0100

    Update recipe list

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import useRecipeStore from "./recipeStore";
 
-function RecipeList() {
+const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
 
   return (
-    <ul>
-      {recipes.map((recipe, index) => (
-        <li key={index}>{recipe}</li>
-      ))}
-    </ul>
+    <div>
+      <h2>Recipe List</h2>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>
+            <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-}
+};
 
 export default RecipeList;
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,10 +1,30 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
-  setRecipes: (newRecipes) => set({ recipes: newRecipes }),
+
+  // Add a new recipe with an id
   addRecipe: (recipe) =>
-    set((state) => ({ recipes: [...state.recipes, recipe] })),
+    set((state) => ({
+      recipes: [
+        ...state.recipes,
+        { id: Date.now().toString(), ...recipe }, // auto-generate ID
+      ],
+    })),
+
+  // Update recipe by id
+  updateRecipe: (id, updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+      ),
+    })),
+
+  // Delete recipe by id
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
 }));
 
 export default useRecipeStore;
