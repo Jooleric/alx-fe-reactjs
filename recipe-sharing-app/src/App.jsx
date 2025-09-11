eb2f4f25237479d6355e7aec142e092d3d4c286e
Jooleric <bernardjoseph7872gmail.com>
2025-09-11 16:01:40 +0100

    Initial commit of recipe-sharing-app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,17 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import RecipeList from "./components/RecipeList";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
+import EditRecipeForm from "./components/EditRecipeForm";
 
 function App() {
   return (
-    <Router>
+    <div>
+      <h1>Recipe Sharing App</h1>
       <Routes>
-        <Route path="/" element={<RecipeList />} />
-        <Route path="/add" element={<AddRecipeForm />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <AddRecipeForm />
+              <RecipeList />
+            </>
+          }
+        />
         <Route path="/recipe/:id" element={<RecipeDetails />} />
+        <Route path="/recipe/:id/edit" element={<EditRecipeForm />} />
       </Routes>
-    </Router>
+    </div>
   );
 }
 
