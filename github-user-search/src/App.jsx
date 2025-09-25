a3c91330e77bd745d24ddf5b0baf0f60c5b40f6b
Jooleric <bernardjoseph7872gmail.com>
2025-09-26 00:05:04 +0100

    "Update project files with Tailwind setup"

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -3,7 +3,7 @@ import Search from "./components/fetchAdvancedUsers";
 export default function App() {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">GitHub User Search</h1>
+      <h1 className="text-2xl font-bold mb-4">GitHub</h1>
       <Search />
     </div>
   );
